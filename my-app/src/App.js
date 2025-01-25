ac6c20fc86a17fbe1fac49b3ad54c9fa5da670f0
CiobanuCiprian9 <ciobanu_ciprian90@yahoo.com>
2025-01-25 13:00:57 +0200

    Modificare fisiere

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,47 @@
+// import React from "react";
+// import NavBar from "./Navbar";
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import ExercisesPage from "./Scripts/ExercisesPage";
+
+// function App() {
+//     return (
+//         // <div>
+//         //   <NavBar />
+//         //     <h1 style={{ color: "white", textAlign: "center", paddingTop: "100px" }}>
+//         //         Welcome to GymWebApp
+//         //     </h1>
+//         // </div>
+//         <Router>
+//             <NavBar />
+//             <Routes>
+//                 <Route path="/" element={
+//                     <h1 style={{ color: "white", textAlign: "center", paddingTop: "100px" }}>
+//                         Welcome to GymWebApp
+//                     </h1>
+//                 } />
+//                 <Route path="/exercises" element={<ExercisesPage />} />
+//             </Routes>
+//         </Router>
+//     );
+// }
+
+// export default App;
+
+
 import React from "react";
-import NavBar from "./Navbar";
+import NavBar from "./Scripts/Navbar";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ExercisesPage from "./Scripts/ExercisesPage";
+import MainPage from "./Scripts/MainPage";
 
 function App() {
     return (
-        // <div>
-        //   <NavBar />
-        //     <h1 style={{ color: "white", textAlign: "center", paddingTop: "100px" }}>
-        //         Welcome to GymWebApp
-        //     </h1>
-        // </div>
         <Router>
             <NavBar />
             <Routes>
-                <Route path="/" element={
-                    <h1 style={{ color: "white", textAlign: "center", paddingTop: "100px" }}>
-                        Welcome to GymWebApp
-                    </h1>
-                } />
+                <Route path="/" element={<MainPage />} />
                 <Route path="/exercises" element={<ExercisesPage />} />
             </Routes>
         </Router>
@@ -27,5 +49,3 @@ function App() {
 }
 
 export default App;
-
-
---
+++ my-app/src/Scripts/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainPage() {
+    return (
+        <div className="main-page">
+            <h1 style={{ color: "white", textAlign: "center", paddingTop: "100px" }}>
+                Welcome to GymWebApp
+            </h1>
+        </div>
+    );
+}
+
+export default MainPage;
